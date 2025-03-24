c8f38f56eff9250cbd694dfb96adcd44b6def9d8
Madhan Kumar <madhankumarbusiness@gmail.com>
2025-03-24 19:04:56 +0530

    video fix

--- src/app/Video/page.tsx
+++ src/app/Video/page.tsx
@@ -65,20 +65,22 @@ export default function Page() {
   }, [seatalert, seat]);
   
   return (
-    <div className="video-container">
-      <video width="420" height="140" controls autoPlay loop>
-        <source src="/Video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-
-      {isModalOpen && (
-        <div className="modal-overlay fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="modal-content bg-gray-800 p-8 rounded-lg shadow-lg w-[80%] max-w-lg">
-            <h2 className="text-xl font-semibold mb-4">Seat Belt Alert</h2>
-            <p className="mb-4">Wear the seat belt</p>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="video-container">
+        <video width="420" height="140" controls autoPlay loop className="mx-auto">
+          <source src="/Video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+  
+        {isModalOpen && (
+          <div className="modal-overlay fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="modal-content bg-gray-800 p-8 rounded-lg shadow-lg w-[80%] max-w-lg">
+              <h2 className="text-xl font-semibold mb-4">Seat Belt Alert</h2>
+              <p className="mb-4">Wear the seat belt</p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
-  );
+  );  
 }
